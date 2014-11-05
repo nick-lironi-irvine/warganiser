66f6d0f594c61418bc2c1d62f08084881e6d6f4d
Nick Lironi-Irvine <nick.a.irvine+github@gmail.com>
2014-11-05 22:46:17 +1300

    Angular controller that POSTs to the server to create a new tournament when create button is clicked

--- org.warganiser.client/src/main/webapp/js/warganiser.js
+++ org.warganiser.client/src/main/webapp/js/warganiser.js
@@ -1,9 +1,18 @@
 'use strict';
 
 /* App Module */
-var tournamentApp = angular.module('tournamentApp', []).controller('TournamentController', ['$scope', function($scope) {
-    $scope.create = function(tournament) {
-        alert('create');
-    };
+var tournamentApp = angular.module('tournamentApp', [])
+    .controller('TournamentController', ['$scope', '$http', function($scope, $http) {
+        $scope.tournament = {};
+
+        $scope.create = function() {
+            $http.post('service/tournament',$scope.tournament)
+                .success(function(data, status, headers, config) {
+                    alert('created tournament with name '+$scope.tournament.name);
+                }).error(function(data, status, headers, config) {
+                        debugger;
+                        alert('fail');
+                });
+        };
 }]);
 
