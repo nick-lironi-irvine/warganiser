67491dd213280b6d5b08dcfe6d3861f7c0df36a5
Nick Lironi-Irvine <nick.a.irvine+github@gmail.com>
2014-11-05 21:40:51 +1300

    Angular-ify the Tournament form

--- org.warganiser.client/src/main/webapp/js/warganiser.js
+++ org.warganiser.client/src/main/webapp/js/warganiser.js
@@ -1,3 +1,9 @@
 'use strict';
 
 /* App Module */
+var tournamentApp = angular.module('tournamentApp', []).controller('TournamentController', ['$scope', function($scope) {
+    $scope.create = function(tournament) {
+        alert('create');
+    };
+}]);
+
