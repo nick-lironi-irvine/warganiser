d1aed53e188cd1c64ad59e3e21bd47501a18a53f
Nick Lironi-Irvine <nick.a.irvine+github@gmail.com>
2014-11-11 22:20:02 +1300

    guice config to wire in JPA persistence with session-per-request

--- org.warganiser.server/src/main/java/org/warganiser/server/servlet/GuiceConfig.java
+++ org.warganiser.server/src/main/java/org/warganiser/server/servlet/GuiceConfig.java
@@ -1,6 +1,5 @@
 package org.warganiser.server.servlet;
 
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,6 +7,8 @@ import org.warganiser.server.core.impl.WarganiserModule;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.persist.PersistFilter;
+import com.google.inject.persist.jpa.JpaPersistModule;
 import com.google.inject.servlet.GuiceServletContextListener;
 import com.sun.jersey.guice.JerseyServletModule;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
@@ -20,12 +21,19 @@ public class GuiceConfig extends GuiceServletContextListener {
 			@Override
 			protected void configureServlets() {
 				install(new WarganiserModule());
+				install(new JpaPersistModule("org.warganiser"));
 
 				// Set init params for Jersey
 				Map<String, String> params = new HashMap<>();
 				params.put("com.sun.jersey.config.property.packages", "org.warganiser.server.resources");
 				params.put("com.sun.jersey.api.json.POJOMappingFeature", "true");
 
+				/*
+				 * Ensure a session-per-http-request strategy to ensure
+				 * EntityManager is available to inject. See
+				 * https://github.com/google/guice/wiki/JPA
+				 */
+				filter("/*").through(PersistFilter.class);
 				// Route all requests through GuiceContainer
 				serve("/*").with(GuiceContainer.class, params);
 			}
