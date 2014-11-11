51f2b585ff1d9388945d2f23827caac5f23721f3
Nick Lironi-Irvine <nick.a.irvine+github@gmail.com>
2014-11-11 22:18:23 +1300

    Remove test instantiation of entity manager in prep for getting guice to instantiate

--- org.warganiser.server/src/main/java/org/warganiser/server/WarganiserServerLauncher.java
+++ org.warganiser.server/src/main/java/org/warganiser/server/WarganiserServerLauncher.java
@@ -1,9 +1,5 @@
 package org.warganiser.server;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -13,42 +9,36 @@ import com.google.inject.servlet.GuiceFilter;
 
 public class WarganiserServerLauncher {
 
-	public static void main(String[] args) throws Exception{
-		
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("org.warganiser");
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		entityManager.getTransaction().commit();
-		entityManager.close();
-		
-        //The port that we should run on can be set into an environment variable
-        //Look for that variable and default to 8180 if it isn't there.
-        String webPort = System.getenv("WARGANISER_SERVER_PORT");
-        if(webPort == null || webPort.isEmpty()) {
-            webPort = "8180";
-        }
+	public static void main(String[] args) throws Exception {
 
-        Server server = new Server(Integer.valueOf(webPort));
-        
-        
-        // Create a servlet context and add the jersey servlet
-        ServletContextHandler sch = new ServletContextHandler(server, "/");
-        
-        // Add our Guice listener that includes our bindings
-        sch.addEventListener(new GuiceConfig());
+		// The port that we should run on can be set into an environment
+		// variable
+		// Look for that variable and default to 8180 if it isn't there.
+		String webPort = System.getenv("WARGANISER_SERVER_PORT");
+		if (webPort == null || webPort.isEmpty()) {
+			webPort = "8180";
+		}
 
-        // Then add GuiceFilter and configure the server to
-        // reroute all requests through this filter.
-        sch.addFilter(GuiceFilter.class, "/*", null);
-        
-        // Must add DefaultServlet for embedded Jetty.
-        // Failing to do this will cause 404 errors.
-        sch.addServlet(DefaultServlet.class, "/");
-        
-        // Start the server
-        server.start();
-        // Wait for server to shutdown
-        server.join();
-    }
+		Server server = new Server(Integer.valueOf(webPort));
+
+		// Create a servlet context and add the jersey servlet
+		ServletContextHandler sch = new ServletContextHandler(server, "/");
+
+		// Add our Guice listener that includes our bindings
+		sch.addEventListener(new GuiceConfig());
+
+		// Then add GuiceFilter and configure the server to
+		// reroute all requests through this filter.
+		sch.addFilter(GuiceFilter.class, "/*", null);
+
+		// Must add DefaultServlet for embedded Jetty.
+		// Failing to do this will cause 404 errors.
+		sch.addServlet(DefaultServlet.class, "/");
+
+		// Start the server
+		server.start();
+		// Wait for server to shutdown
+		server.join();
+	}
 
 }
