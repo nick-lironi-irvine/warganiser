026a05da112b4b348c1d1061bd7f02b186e78c81
Nick Lironi-Irvine <nick.a.irvine+github@gmail.com>
2014-11-11 22:19:03 +1300

    start annotating tournament with JPA annotations

--- org.warganiser.server/src/main/java/org/warganiser/server/core/Tournament.java
+++ org.warganiser.server/src/main/java/org/warganiser/server/core/Tournament.java
@@ -1,16 +1,37 @@
 package org.warganiser.server.core;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "Tournament", uniqueConstraints = @UniqueConstraint(columnNames = { "name" }))
 public class Tournament {
+
+	@Id
+	private Long id;
 
 	private String name;
 
 	public Tournament(String name) {
-		this.name = name;
+		this.setName(name);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-}
+	public void setName(String name) {
+		this.name = name;
+	}
 
+}
