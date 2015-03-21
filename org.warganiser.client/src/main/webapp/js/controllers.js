e6ed4af77478186bd7d70989d91fa4f39a935644
Nick Lironi-Irvine <nick.lironi-irvine@orionhealth.com>
2015-03-21 20:15:29 +1300

    Create a new controller for the Tournament editing screen Pass scope from the first controller to the tournament editing controller via route params

--- org.warganiser.client/src/main/webapp/js/controllers.js
+++ org.warganiser.client/src/main/webapp/js/controllers.js
@@ -3,22 +3,32 @@
 var warganiserControllers = angular.module('warganiserControllers', []);
 
 /* Controller for Tournament creation screen*/
-warganiserControllers.controller('TournamentController', ['$scope', '$location', 'tournamentService', function($scope, $location, tournamentService) {
-    $scope.tournament = {};
+warganiserControllers.controller('TournamentController', ['$scope', '$location', 'tournamentService',
+    function($scope, $location, tournamentService) {
+        $scope.tournament = {};
 
-    $scope.create = function(tournament) {
+        $scope.create = function(tournament) {
 
-        var promise = tournamentService.create(tournament).$promise;
+            var promise = tournamentService.create(tournament).$promise;
 
-        promise.then(function(tournament) {
-            //On success, update the tournament in scope and redirect to the tournament editing page
-            $scope.tournament = tournament;
-            $location.path('/warganiser/' + tournament.name);
-        }, function(errorDTO) {
-            //TODO on error, display the error messages and stay on this page
-            alert('failed');
-        });
+            promise.then(function(tournament) {
+                //On success, update the tournament in scope and redirect to the tournament editing page
+                $scope.tournament = tournament;
+                $location.path('/warganiser/' + tournament.name);
+            }, function(errorDTO) {
+                //TODO on error, display the error messages and stay on this page
+                alert('failed');
+            });
 
-    };
+        };
 
-}]);
+    }
+]);
+
+/* Controller for Tournament editing screen*/
+warganiserControllers.controller('TournamentEditController', ['$scope', '$routeParams',
+    function($scope, $routeParams) {
+
+        $scope.tournamentName = $routeParams.tournamentName;
+    }
+]);
